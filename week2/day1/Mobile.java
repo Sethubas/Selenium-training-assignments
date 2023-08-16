b9ac162b0e05258cb9ed2ee1900a91c614125b26
Sethubas <141891569+Sethubas@users.noreply.github.com>
2023-08-17 01:12:41 +0530

    Add files via upload

---
+++ week2/day1/Mobile.java
@@ -0,0 +1,23 @@
+package week2.day1;
+
+public class Mobile {
+
+	public void sendSMS(String input) {
+		System.out.println(input);
+	}
+	
+	protected void allowVoiceCall(){
+		System.out.println("Allow Voice Call");
+	}
+	
+private void takeVideo() {
+	System.out.println("Hello World");
+}
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week2/day1/Tab.java
@@ -0,0 +1,13 @@
+package week2.day1;
+
+public class Tab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Mobile obj=new Mobile();
+		obj.sendSMS("Hello TestLeaf");
+		obj.allowVoiceCall();
+	}
+
+}
